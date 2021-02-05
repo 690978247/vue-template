436a9672b70feb4a189d9c7a18f308c3ce2eb9ba
xulk <laike.xu@shengyc.com>
2021-02-05 17:22:29 +0800

    add tags

--- src/settings.js
+++ src/settings.js
@@ -8,6 +8,12 @@ module.exports = {
    */
   fixedHeader: false,
 
+    /**
+   * @type {boolean} true | false
+   * @description Whether need tagsView
+   */
+  tagsView: true,
+
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
